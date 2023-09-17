704c1923a8d2de78b24c82dde3fa93a7bb221ecb
Thacio Alves <thaccio.alves@gmail.com>
2023-09-17 20:58:05 -0300

    rotas, css e html

--- src/app/administrador/administrador-routing.module.ts
+++ src/app/administrador/administrador-routing.module.ts
@@ -7,7 +7,7 @@ import { HomeComponent } from './components/home/home.component';
 const routes: Routes = [
   { path: 'cadastro', component: CadastroComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'home', component: HomeComponent },
+  { path: 'admin', component: HomeComponent },
 ];
 
 @NgModule({

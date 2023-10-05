ca230cc2842796b9a0fbd90f76493e317c691f79
Thacio Alves <thaccio.alves@gmail.com>
2023-10-05 18:27:22 -0300

    sidebar

--- src/app/administrador/flor/components/flor-delete/flor-delete.component.ts
+++ src/app/administrador/flor/components/flor-delete/flor-delete.component.ts
@@ -6,17 +6,9 @@ import {MatButtonModule} from '@angular/material/button';
   selector: 'app-flor-delete',
   templateUrl: './flor-delete.component.html',
   styleUrls: ['./flor-delete.component.css'],
-  imports: [MatButtonModule, MatDialogModule],
-  standalone: true,
+
 })
 
-export class DialogElementsExample {
-  constructor(public dialog: MatDialog) {}
-
-  openDialog() {
-    this.dialog.open(DialogElementsExampleDialog);
-  }
-}
 export class FlorDeleteComponent {}
 
 

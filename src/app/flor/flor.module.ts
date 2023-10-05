81368e8ab7e5ce078f0533ea1371d90ffc56036e
Thacio Alves <thaccio.alves@gmail.com>
2023-10-05 13:05:05 -0300

    module flor, fornecedor e usuario

---
+++ src/app/flor/flor.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FlorRoutingModule } from './flor-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FlorRoutingModule
+  ]
+})
+export class FlorModule { }

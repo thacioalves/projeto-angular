e09a88a0b21a17c9a2c8a1e51c4e08478a1520a6
Thacio Alves <thaccio.alves@gmail.com>
2023-10-06 19:45:33 -0300

    services desconto, estoque

--- src/app/services/desconto.service.ts
+++ src/app/services/desconto.service.ts
@@ -1,9 +1,46 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Desconto } from '../models/desconto';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DescontoService {
+  private baseURL: string = 'http://localhost:8080';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  findAll(): Observable<Desconto[]> {
+    return this.http.get<Desconto[]>(`${this.baseURL}/descontos`);
+  }
+
+  findById(id: string): Observable<Desconto> {
+    return this.http.get<Desconto>(`${this.baseURL}/descontos/${id}`);
+  }
+
+  save(desconto: Desconto): Observable<Desconto> {
+    const obj = {
+      valor: desconto.valor,
+      descricao: desconto.descricao,
+    };
+    return this.http.post<Desconto>(`${this.baseURL}/descontos`, obj);
+  }
+
+  update(desconto: Desconto): Observable<Desconto> {
+    const obj = {
+      valor: desconto.valor,
+      descricao: desconto.descricao,
+    };
+    return this.http.put<Desconto>(
+      `${this.baseURL}/descontos/${desconto.id}`,
+      obj
+    );
+  }
+
+  delete(desconto: Desconto): Observable<any> {
+    return this.http.delete<Desconto>(
+      `${this.baseURL}/descontos/${desconto.id}`
+    );
+  }
 }
--- src/app/services/estoque.service.ts
+++ src/app/services/estoque.service.ts
@@ -1,9 +1,44 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Estoque } from '../models/estoque';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EstoqueService {
+  private baseURL: string = 'http://localhost:8080';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  findAll(): Observable<Estoque[]> {
+    return this.http.get<Estoque[]>(`${this.baseURL}/estoques`);
+  }
+
+  findById(id: string): Observable<Estoque> {
+    return this.http.get<Estoque>(`${this.baseURL}/estoques/${id}`);
+  }
+
+  save(estoque: Estoque): Observable<Estoque> {
+    const obj = {
+      quantidadeProduto: estoque.quantidadeProduto,
+      produto: estoque.idProduto,
+    };
+    return this.http.post<Estoque>(`${this.baseURL}/estoques`, obj);
+  }
+
+  update(estoque: Estoque): Observable<Estoque> {
+    const obj = {
+      quantidadeProduto: estoque.quantidadeProduto,
+      produto: estoque.idProduto,
+    };
+    return this.http.put<Estoque>(
+      `${this.baseURL}/estoques/${estoque.id}`,
+      obj
+    );
+  }
+
+  delete(estoque: Estoque): Observable<any> {
+    return this.http.delete<Estoque>(`${this.baseURL}/estoques/${estoque.id}`);
+  }
 }
